4adcc42ea3a9544a78aa9adb2800081207599eb3
Alexlife9 <crashmx@inbox.ru>
2022-10-06 18:21:15 +0300

    перенос в другую папку

--- src/main/java/ru/job4j/dreamjob/controller/PostController.java
+++ src/main/java/ru/job4j/dreamjob/controller/PostController.java
@@ -1,8 +1,9 @@
-package ru.job4j.dreamjob.model;
+package ru.job4j.dreamjob.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.dreamjob.model.PostStore;
 
 /**
  * Thymeleaf, Циклы
