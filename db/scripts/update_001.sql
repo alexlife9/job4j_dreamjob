19aa687d82899ff9380da172d9f46b3968a7dfa9
Alexlife9 <crashmx@inbox.ru>
2022-10-16 20:17:06 +0300

    Тестирование базы данных. Liquibase H2

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,7 +1,31 @@
-CREATE TABLE post (
-   id SERIAL PRIMARY KEY,
-   name VARCHAR(100),
-   description TEXT,
-   created DATE,
-   city_id INT
+CREATE TABLE IF NOT EXISTS cities
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS post
+(
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(100),
+    description TEXT,
+    created     DATE,
+    city_id     INT REFERENCES cities (id)
+);
+
+CREATE TABLE IF NOT EXISTS candidate
+(
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(100),
+    description TEXT,
+    created     DATE,
+    city_id     INT REFERENCES cities (id)
+);
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id       SERIAL PRIMARY KEY,
+    name     VARCHAR(100) UNIQUE,
+    email    VARCHAR(100) UNIQUE,
+    password TEXT
 );
