57725a13ee43db9cf398809779a9d7f5a6245340
Alexlife9 <crashmx@inbox.ru>
2022-10-23 19:29:21 +0300

    Проверка доступа с помощью фильтра

--- src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
+++ src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
@@ -1,10 +1,12 @@
 package ru.job4j.dreamjob.filter;
 
 import org.springframework.stereotype.Component;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Set;
 
 import static java.util.Objects.isNull;
 
@@ -12,13 +14,22 @@ import static java.util.Objects.isNull;
  * Проверка доступа с помощью фильтра
  *
  * @author Alex_life
- * @version 1.0
- * @since 21.10.2022
+ * @version 2.0
+ * @since 23.10.2022
  */
 @Component
 public class AuthFilter implements Filter {
+    private static final String PAGES_FILTER =
+            String.format("%S%S%S%S",
+                    "loginPage", "formAddUser",
+                    "login", "registration");
     private static final String USER = "user";
     private static final String LOGIN_PAGE = "/loginPage";
+    private final Set<String> pages = Set.of(PAGES_FILTER);
+
+    private boolean containPages(String uri) {
+        return pages.stream().allMatch(uri::endsWith);
+    }
 
     @Override
     public void doFilter(
@@ -28,9 +39,7 @@ public class AuthFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
         String uri = req.getRequestURI();
-        if (uri.endsWith("loginPage") || uri.endsWith("formAddUser") || uri.endsWith("index")
-        || uri.endsWith("login") || uri.endsWith("registration") || uri.endsWith("addUser") || uri.endsWith("success")
-                || uri.endsWith("fail")) {
+        if (containPages(uri)) {
             chain.doFilter(req, res);
             return;
         }
